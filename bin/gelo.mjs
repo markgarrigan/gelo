61ee6917c08d455f5d0ec51787a2ca9aae223af5
Mark Garrigan <mark@markgarrigan.com>
2020-09-15 08:15:02 -0500

    handle deletes and build js with esbuild

--- bin/gelo.mjs
+++ bin/gelo.mjs
@@ -3,49 +3,38 @@
 import cmd from 'commander'
 import fs from 'fs'
 import ffif from 'fast-find-in-files'
-import riot from '@riotjs/compiler'
 import sass from 'sass'
 import imagemin from 'imagemin'
 import imageminJpegtran from 'imagemin-jpegtran'
 import imageminPngquant from 'imagemin-pngquant'
 import chokidar from 'chokidar'
+import esbuild from 'esbuild'
+import del from 'del'
 
+const { buildSync } = esbuild
 const { fastFindInFiles } = ffif
-const {program} = cmd
+const { program } = cmd
 
-riot.registerPreprocessor('css', 'sass', function(code, { options }) {
-    const { file } = options
+let hrstart
 
-    console.log('Compile the sass code in', file)
-
-    const {css} = sass.renderSync({
-        data: code
-    })
-
-    return {
-        code: css.toString(),
-        map: null
-    }
-})
-
-const opts = new function() {
+const opts = new function () {
     this.sep = '/'
     this.partial = '_'
     this.event = 'change'
+    this.target = 'es5'
     this.ext = {
         html: '.html',
         js: '.js',
-        riot: '.riot',
         css: '.css',
-        scss: '.scss'     
+        scss: '.scss'
     }
     this.paths = {
         root: 'src',
         dest: 'dist',
-        riot: `static${this.sep}js${this.sep}tags`,
         images: `static${this.sep}images`,
         files: `static${this.sep}files`,
-        css: `static${this.sep}css`
+        css: `static${this.sep}css`,
+        js: `static${this.sep}js`
     }
 }
 
@@ -57,19 +46,19 @@ const rootDir = (path) => {
     return `${opts.paths.root}${path.split(`${opts.sep}${opts.paths.root}`).pop()}`
 }
 
-const ll = (start,filter) => {
+const ll = (start, filter) => {
     let listing = []
-    
-    if (!fs.existsSync(start)){
+
+    if (!fs.existsSync(start)) {
         return []
     }
-    
-    const files=fs.readdirSync(start)
-    
+
+    const files = fs.readdirSync(start)
+
     files.forEach(file => {
         const path = `${start}${opts.sep}${file}`
         if (fs.lstatSync(path).isDirectory()) {
-            listing = [...listing, ...ll(path,filter)]
+            listing = [...listing, ...ll(path, filter)]
             return
         }
         if (path.includes(filter)) {
@@ -79,7 +68,7 @@ const ll = (start,filter) => {
         }
     })
     return listing
-};
+}
 
 const currentDir = (path) => {
     let dir = path.split(opts.sep)
@@ -91,12 +80,12 @@ const relativeDir = (path) => {
     return path.replace(`${opts.paths.root}${opts.sep}`, '')
 }
 
-const doInclude = ({parent, child, needle}) => {
-    const re = new RegExp(needle,"g")
+const doInclude = ({ parent, child, needle }) => {
+    const re = new RegExp(needle, "g")
     return parent.replace(re, child)
 }
 
-const doInject = ({content, inject, value}) => {
+const doInject = ({ content, inject, value }) => {
     const re = new RegExp(inject, "g")
     return content.replace(re, value)
 }
@@ -107,7 +96,7 @@ const findAllGeloFiles = (path) => {
 
 const lookForGelo = (file) => {
     let fileContent = fs.readFileSync(file, 'utf8')
-    const re = RegExp('<!--gelo(.*)-->','g')
+    const re = RegExp('<!--gelo(.*)-->', 'g')
     return Array.from(fileContent.matchAll(re), m => m[0])
 }
 
@@ -147,7 +136,7 @@ const updateParams = (params, content) => {
             [...new Set(injects)].forEach(inject => {
                 const param = inject.split('.').pop().replace('}', '')
                 const value = data[param]
-                content = doInject({content, inject, value})
+                content = doInject({ content, inject, value })
             })
         }
         return content
@@ -156,26 +145,28 @@ const updateParams = (params, content) => {
     }
 }
 
-const updateSinglePage = (page) => {   
-    const dir = currentDir(page)    
+const updateSinglePage = (page, reporting = true) => {
+    const dir = currentDir(page)
     const includes = lookForGelo(page)
     let pageContent = fs.readFileSync(`${process.cwd()}${opts.sep}${page}`, 'utf8')
     if (!includes.length) {
         if (fileName(page)[0] != opts.partial) {
             moveToDest(page, pageContent)
+            if (reporting)
+                report(process.hrtime(hrstart))
         }
         return pageContent
     }
     for (const include of includes) {
         const gelo = geloDetails(include, dir)
         const file = `${process.cwd()}${opts.sep}${gelo.path}`
-        const isFile = fs.existsSync(file)        
+        const isFile = fs.existsSync(file)
         let content = isFile ? updateSinglePage(gelo.path) : gelo.line.replace('<!--gelo', '<!--missing')
-        
+
         if (gelo.params) {
             content = updateParams(gelo.params, content)
         }
-        
+
         pageContent = isFile ? doInclude({
             parent: pageContent,
             child: content,
@@ -184,35 +175,30 @@ const updateSinglePage = (page) => {
     }
     if (fileName(page)[0] != opts.partial) {
         moveToDest(page, pageContent)
+        if (reporting)
+            report(process.hrtime(hrstart))
     }
     return pageContent
 }
 
-const updateAllPages = (geloPath) => {
+const updateAllPages = (geloPath, reporting = true) => {
     findAllGeloFiles(opts.paths.root)
-    .filter(possible => possible.filePath.split(opts.sep).pop()[0] != opts.partial)
-    .forEach(possible => {
-        possible.queryHits
-        .filter(hit => hit.line.includes(fileName(geloPath)))
-        .forEach(hit => {
-            const gelo = geloDetails(hit.line, currentDir(possible.filePath))
-            if (geloPath == gelo.path) {
-                updateSinglePage(possible.filePath)
-            }
-        });
-    });    
+        .filter(possible => possible.filePath.split(opts.sep).pop()[0] != opts.partial)
+        .forEach(possible => {
+            possible.queryHits
+                .filter(hit => hit.line.includes(fileName(geloPath)))
+                .forEach(hit => {
+                    const gelo = geloDetails(hit.line, currentDir(possible.filePath))
+                    if (geloPath == gelo.path) {
+                        updateSinglePage(possible.filePath, false)
+                    }
+                })
+        })
+    if (reporting)
+        report(process.hrtime(hrstart))
 }
 
-const compileRiot = (path) => {
-    const component = fs.readFileSync(`${process.cwd()}${opts.sep}${path}`, 'utf8')
-    const {code, map} = riot.compile(component)
-    moveToDest(
-        `${opts.paths.root}${opts.sep}${opts.paths.riot}${opts.sep}${relativeDir(path).replace(opts.ext.riot, opts.ext.js)}`,
-        code
-    )
-}
-
-const compileCSS = () => {
+const compileCSS = (reporting = true) => {
     const paths = ll(`${process.cwd()}${opts.sep}${opts.paths.root}${opts.sep}${opts.paths.css}`, 'css')
     paths.forEach(path => {
         if (path.includes(opts.ext.css)) {
@@ -220,15 +206,30 @@ const compileCSS = () => {
             moveToDest(path, css)
         }
         if (path.includes(opts.ext.scss)) {
-            const {css,map} = sass.renderSync({
+            const { css, map } = sass.renderSync({
                 file: path
             })
             moveToDest(path.replace(opts.ext.scss, opts.ext.css), css.toString())
         }
     })
+    if (reporting)
+        report(process.hrtime(hrstart))
 }
 
-const compressImages = async (path) => {
+const buildJS = (reporting = true) => {
+    const paths = ll(`${process.cwd()}${opts.sep}${opts.paths.root}${opts.sep}${opts.paths.js}`, 'js')
+    buildSync({
+        entryPoints: paths,
+        outdir: `${process.cwd()}${opts.sep}${opts.paths.dest}${opts.sep}${opts.paths.js}`,
+        minify: true,
+        bundle: true,
+        target: program.target
+    })
+    if (reporting)
+        report(process.hrtime(hrstart))
+}
+
+const compressImages = async (path, reporting = true) => {
     await imagemin([path], {
         destination: `${opts.paths.dest}${opts.sep}${opts.paths.images}`,
         plugins: [
@@ -238,9 +239,11 @@ const compressImages = async (path) => {
             })
         ]
     })
+    if (reporting)
+        report(process.hrtime(hrstart))
 }
 
-const copyFile = (path) => {
+const copyFile = (path, reporting = true) => {
     let folders = path.split(opts.sep).slice(0, -1)
     folders.splice(0, 1, opts.paths.dest)
     folders.reduce(
@@ -253,58 +256,65 @@ const copyFile = (path) => {
         }
     )
     fs.copyFileSync(path, `${folders.join(opts.sep)}${opts.sep}${fileName(path)}`)
+    if (reporting)
+        report(process.hrtime(hrstart))
 }
 
-const copyFiles = (paths) => {
+const copyFiles = (paths, reporting) => {
     paths.forEach(path => {
-        copyFile(path)
-    });
+        copyFile(path, false)
+    })
+    if (reporting)
+        report(process.hrtime(hrstart))
 }
 
 const writeFileSyncRecursive = (filename, content, charset) => {
-    let filepath = filename.replace(/\\/g,opts.sep);
-    let root = '';
-    if (filepath[0] === opts.sep) { 
-        root = opts.sep; 
-        filepath = filepath.slice(1);
-    } 
-    else if (filepath[1] === ':') { 
-        root = filepath.slice(0,3);
-        filepath = filepath.slice(3); 
+    let filepath = filename.replace(/\\/g, opts.sep)
+    let root = ''
+    if (filepath[0] === opts.sep) {
+        root = opts.sep
+        filepath = filepath.slice(1)
     }
-    const folders = filepath.split(opts.sep).slice(0, -1);
+    else if (filepath[1] === ':') {
+        root = filepath.slice(0, 3)
+        filepath = filepath.slice(3)
+    }
+    const folders = filepath.split(opts.sep).slice(0, -1)
     folders.reduce(
         (acc, folder) => {
-            const folderPath = acc + folder + opts.sep;
+            const folderPath = acc + folder + opts.sep
             if (!fs.existsSync(folderPath)) {
-                fs.mkdirSync(folderPath);
+                fs.mkdirSync(folderPath)
             }
             return folderPath
         },
         root
-    );
-    fs.writeFileSync(root + filepath, content, charset);
+    )
+    fs.writeFileSync(root + filepath, content, charset)
 }
 
 const clear = () => {
-    process.stdout.write('\u001B[2J\u001B[0;0f')
+    process.stdout.write('\u001B[2J\u001B[00f')
+}
+
+const clean = () => {
+    const dir = `${process.cwd()}${opts.sep}${opts.paths.dest}`
+    del.sync([dir])
 }
 
 const startTime = () => {
     clear()
-    return process.hrtime()    
+    return process.hrtime()
 }
 
 const report = (hrend) => {
-    // clear()
     console.info('⭐️Finished: %ds %dms', hrend[0], hrend[1] / 1000000)
-    // process.exit()
 }
 
 const added = async (path) => {
-    const hrstart = startTime()
+    hrstart = startTime()
     if (path.includes(opts.paths.images)) {
-        await compressImages(path)
+        compressImages(path)
     }
     if (path.includes(opts.paths.files)) {
         copyFile(path)
@@ -313,7 +323,7 @@ const added = async (path) => {
 }
 
 const changed = (path) => {
-    const hrstart = startTime()
+    hrstart = startTime()
     if (path.includes(opts.paths.files)) {
         copyFile(path)
     }
@@ -329,67 +339,40 @@ const changed = (path) => {
     if (path.includes(opts.ext.css) || path.includes(opts.ext.scss)) {
         compileCSS()
     }
-    if (path.includes(opts.ext.riot)) {
-        program.riot ? compileRiot(path) : copyFile(path)
+    if (path.includes(opts.ext.js)) {
+        buildJS()
     }
-    report(process.hrtime(hrstart))
 }
 
 const unlinked = (path) => {
-    // if (changedFileName.includes(opts.ext.html)) {
-    //     if (changedFileName[0] == opts.partial) {
-    //         const needle = changedFileName
-    //         const results = fastFindInFiles(opts.paths.root, needle)
-    //         if (results.length) {
-    //             console.log(`${changedFileName} is used in:`);
-                
-    //             results.forEach(result => {
-    //                 console.log(result.filePath);
-    //             });
-                
-    //             console.log(`File${results.length > 1 ? 's' : ''} skipped.`)
-    //         }
-    //     } else {
-    //         const dile = options.path.replace(opts.paths.root, opts.paths.dest)
-    //         console.log(dile);
-            
-    //         // if (fs.existsSync(dile)) {
-    //         //     fs.unlinkSync(dile)
-    //         // }
-    //     }
-    // }
-    
-    // if (changedFileName.includes('.riot')) {
-    //     const dile = options.path
-    //     .replace(opts.paths.root, `${opts.paths.dest}${opts.sep}${opts.paths.riot}`)
-    //     .replace(opts.ext.riot, opts.ext.js)
-    //     const path = `${process.cwd()}${opts.sep}${dile}`
-    //     if (fs.existsSync(path)) {
-    //         // shell.rm(path)
-    //         // try {
-    //         //     // fs.unlinkSync(path)
-    //         //     //file removed
-    //         //   } catch(err) {
-    //         //     console.error(err)
-    //         //   }
-    //     }
-    // }
+    const dile = path.replace(opts.paths.root, opts.paths.dest)
+    if (path.includes(opts.ext.html) || path.includes(opts.paths.files) || path.includes(opts.ext.js)) {
+        if (fs.existsSync(dile)) {
+            console.log(`Removed: ${dile}`)
+            del.sync([dile]);
+        }
+    }
+    if (path.includes(opts.ext.css) || path.includes(opts.ext.scss)) {
+        compileCSS()
+    }
+    if (path.includes(opts.ext.js)) {
+        buildJS()
+    }
 }
 
-const build = async (paths, options) => {
+const build = async (paths) => {
     try {
-        const hrstart = startTime()
+        hrstart = startTime()
+        clean()
         paths.forEach(path => {
             if (path.includes(opts.ext.html)) {
-                updateSinglePage(path)
-            }
-            if (path.includes(opts.ext.riot)) {
-                program.riot ? compileRiot(path) : copyFile(path)
+                updateSinglePage(path, false)
             }
         })
-        compileCSS()
-        copyFiles(paths.filter(path => path.includes(opts.paths.files)))
-        await compressImages(`${opts.paths.root}${opts.sep}${opts.paths.images}${opts.sep}*.{jpg,png}`)
+        compileCSS(false)
+        copyFiles(paths.filter(path => path.includes(opts.paths.files)), false)
+        compressImages(`${opts.paths.root}${opts.sep}${opts.paths.images}${opts.sep}*.{jpg,png}`, false)
+        buildJS(false)
         report(process.hrtime(hrstart))
     } catch (error) {
         console.error(error)
@@ -397,38 +380,40 @@ const build = async (paths, options) => {
     }
 }
 
-const dev = async (options) => {
+const dev = async () => {
     clear()
     const watcher = chokidar.watch(opts.paths.root, {
         ignoreInitial: true,
         persistent: true
-    });
+    })
     // Something to use when events are received.
-    const log = console.log.bind(console);
+    const log = console.log.bind(console)
     // Add event listeners.
     watcher
-    .on('add', path => added(path))
-    .on('change', path => changed(path))
-    .on('unlink', path => unlinked(path))
-    .on('error', error => {
-        log(`Watcher error: ${error}`)
-        process.exit()
-    })
+        .on('add', path => added(path))
+        .on('change', path => changed(path))
+        .on('unlink', path => unlinked(path))
+        .on('error', error => {
+            log(`Watcher error: ${error}`)
+            process.exit()
+        })
+    log('Initial build...')
+    await build(
+        ll(`${process.cwd()}${opts.sep}${opts.paths.root}`, '.'),
+        opts
+    )
     log('Gelo is ready...')
 }
-
-program
-    .option('--no-riot', 'do not compile riot tags to javascript')
 
 program
     .command('build')
     .description('build all source files to destination directory')
     // .option('-s, --source <source>', 'the source directory to build from', opts.paths.root)
     // .option('-d, --destination <destination>', 'the destination directory of the build', opts.paths.dest)
-    .action((source, destination) => {
+    .option('--js-target <target>', 'Environment target (e.g. es5, es6, es2017, chrome58, firefox57, safari11, edge16, node10, default esnext)', opts.target)
+    .action(() => {
         build(
-            ll(`${process.cwd()}${opts.sep}${opts.paths.root}`, '.'),
-            opts
+            ll(`${process.cwd()}${opts.sep}${opts.paths.root}`, '.')
         )
     })
 
@@ -437,8 +422,9 @@ program
     .description('build a source file to destination directory')
     // .option('-s, --source <source>', 'the source directory of your development files', opts.paths.root)
     // .option('-d, --destination <destination>', 'the destination directory of the build', opts.paths.dest)
-    .action((opts) => {
-        dev(opts)
+    .option('--js-target <target>', 'Environment target (e.g. es5, es6, es2017, chrome58, firefox57, safari11, edge16, node10, default esnext)', opts.target)
+    .action(() => {
+        dev()
     })
 
 program.parse(process.agv)
